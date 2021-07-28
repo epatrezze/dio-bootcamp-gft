32fd06010f779810848a95a9baf35b00b34a0192
bannzp <evandro.patrezze@hotmail.com>
2021-07-28 20:22:57 -0300

    Organização de codigo de implementações features java 11.

---
+++ src/java11/StringBlankExemplo.java
@@ -0,0 +1,8 @@
+package java11;
+
+public class StringBlankExemplo {
+    public static void main(String[] args) {
+        String espaco = "           ";
+        System.out.println(espaco.isBlank());
+    }
+}
---
+++ src/java11/StringRepeat.java
@@ -0,0 +1,8 @@
+package java11;
+
+public class StringRepeat {
+    public static void main(String[] args) {
+        String nome = "Joao ";
+        System.out.println("StringFlood:> "+nome.repeat(10));
+    }
+}
